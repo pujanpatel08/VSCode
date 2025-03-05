88ec8c0930cea6ae22e3dc82397f033e0551056f
Pujan Patel <117176870+pujanpatel08@users.noreply.github.com>
2025-03-04 23:54:23 -0500

    Update CountE.java

--- JavaProjects/CSCourses/CS1114/Practice/CountE.java
+++ JavaProjects/CSCourses/CS1114/Practice/CountE.java
@@ -8,7 +8,7 @@ public class CountE {
 		// TODO Auto-generated method stub
 		ArrayList<String> list = new ArrayList<>();
 		list.add("Patel");
-		list.add("Jeremy");
+		list.add("Breeze");
 		list.add("Injure");
 		list.add("Table");
 		
