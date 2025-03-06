9b4acabeeec9381ed7b078018b5b952a50c8abe8
Pujan Patel <pujanpatel8@gmail.com>
2025-03-06 05:52:35 -0500

    changes

--- JavaProjects/CS2114/Stacks.java
+++ JavaProjects/CS2114/Stacks.java
@@ -40,6 +40,7 @@ class Stack<T> {
 }
 public class Stacks {
     public static void main(String[] args) {
+        // TODO Auto-generated method stub
         Stack<String> stack = new Stack<>();
         stack.push("1000");
         stack.push("2000");
