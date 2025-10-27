79b81eec2af7e994f01c3eff9c653d70934c0526
Pujan Patel <pujanpatel8@gmail.com>
2025-10-27 16:12:41 -0400

    morePractice

---
+++ CS2505/practice/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    int x = 10;
+    
+
+    printf("This is the value of x: %d\n", x);
+    printf("This is the memory address of x: %p", &x);
+
+    return 0;
+}
--- CS2505/practice/pointers/pointers.c
+++ CS2505/practice/pointers/pointers.c
@@ -1,2 +1,15 @@
 #include <stdio.h>
 
+int main() {
+
+    int age = 21; //variable has a value and an address
+    int *pAge = &age;
+
+    printf("This is the value of age: %d\n", age);
+    printf("This is the memory address of age: %p\n", &age);
+
+    printf("This is the value of age: %d\n", *pAge);
+    printf("This is the memory address of age: %p\n", pAge);
+
+    return 0;
+}
